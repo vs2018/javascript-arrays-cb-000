a3f4691390299aa5dd715377ca8eaf11996a1ef7
Vishal Sharma <vsh1819@gmail.com>
2018-08-03 09:10:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ var removeElementFromBeginningOfArray = (array) => {
 var destructivelyRemoveElementFromEndOfArray = (array) => array.pop()
 
 var removeElementFromEndOfArray = (array) => {
-  array.slice(0, iceCreams.length - 1)
+  array.slice(0, array.length - 1)
   return array
 }
 
