3afb6f01f77c3a0c5ebf0b63c569054a069fa62c
Vishal Sharma <vsh1819@gmail.com>
2018-08-03 09:22:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,7 @@ var destructivelyRemoveElementFromBeginningOfArray = (array) => {
 }
 
 var removeElementFromBeginningOfArray = (array) => {
-  array.slice(0, array.length - 1)
+  array.splice(1)
 
 }
 
