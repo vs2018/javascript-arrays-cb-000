2ad91ad56eda83f1719e29778912c17dfe51c412
Vishal Sharma <vsh1819@gmail.com>
2018-08-03 08:49:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,16 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 // var addElementToBeginningOfArray = (a, element) => {
 //   [element, ...a]
 // }
-// 
+//
 // var destructivelyAddElementToBeginningOfArray = (array, element) => {
 //   array.unshift(element)
 // }
+
+function addElementToBeginningOfArray (a, e) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray (a, e) {
+  
+}
+
